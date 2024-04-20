fc70000a7826e6a7f3dede070d415e2a02d4ae39
yukehan.non-employee <yukehan.non-employee@inspirai.com>
2024-04-20 15:26:54 +0800

    [细节修改]

--- Pump_Controller.py
+++ Pump_Controller.py
@@ -1,18 +1,22 @@
-from asyncio import sleep
-import binascii
 import sys
 import time
 import serial
 
+# 请务必善于使用log打印错误信息
+
+# 命令和选项的映射
+const = {
+    # TODO
+}
+
 # 创建一个串行端口对象
 ser = serial.Serial()
 
-# 构建泵通讯协议的命令字符串
-# TODO
-
 # 设置串行端口的参数
 try:
-    ser = serial.Serial('COM4', 9600)
+    # 使用之前使用FindCOM.py找到的串口号
+    # 根据实际情况修改COM名和波特率
+    ser = serial.Serial('COM4', 9600) 
 except serial.SerialException as e:
     print(f"参数设置有误 {e}\n")
 # ser.port = 'COM1'  # 串行端口名称，根据实际情况修改
@@ -23,7 +27,6 @@ except serial.SerialException as e:
 
 # 打开串行端口 同时异常排查
 try:
-    # ser.open()
     print(f"RS232成功打开 和 {ser.port} 建立连接\n")
 except serial.SerialException as e:
     print(f"无法通过RS232打开端口: {e}\n")
@@ -33,36 +36,41 @@ except serial.SerialException as e:
 while True:
     time.sleep(1)
     # 按照提示输入参数
-    answer = input('what to do (start_pump(P1,G1,1\n), stop_pump(P1,G1,0\n), flow(P1,S3,XXXXX\n), query_pressure(P1,Q2\n), return(), or set_pressure(P1,S6,mmm.nnn\n)) : \n') 
+    answer = input('what to do (start_pump(P1,G1,1), stop_pump(P1,G1,0), set_flow(P1,S3,XXXXX), query_pressure(P1,Q2), return(), or set_pressure(P1,S6,mmm.nnn)) : \n') 
     if answer == 'P1,G1,1':
-        # TODO
         s = ser.write("P1,G1,1\r\n".encode('utf-8'))
         print(s)
         data = ser.read(10) # 读取10个字节
-        print(f'开泵\n response: {data}\n') 
-        break 
+        print(f'开泵 response: {data}\n') 
+        continue 
     elif answer == 'P1,G1,0':
-        # TODO
         ser.write('P1,G1,0\r\n'.encode("utf-8"))
         data = ser.read(10)  # 读取10个字节
         print(f'关泵 respnse: {data}\n')
-    elif answer == 'flow':
-        # TODO
-        print('流量设置\n')
+        continue
+    elif answer == 'set_flow':
+        flow = input('请输入流量值 XX.XXX ml/min: ')
+        # TODO 参数校验
+        ser.write(f'P1,S3,{flow}\r\n'.encode('utf-8'))
         data = ser.read(10)  # 读取10个字节
+        print(f'流量设置 respnse: {data}\n')
+        continue
     elif answer == 'query_pressure':
-        # TODO
-        print('压力查询\n')
+        ser.write('P1,Q2\r\n'.encode('utf-8'))
         data = ser.read(10)  # 读取10个字节
+        print(f'压力查询 respnse: {data}\n')
+        continue 
     elif answer == 'return':
         # TODO
         print('返回值\n')
         data = ser.read(10)  # 读取10个字节
+        continue
     elif answer == '设置压力':
         # TODO
-        # 注意泵是否支持压力设设置
-        print('压力设置\n')
+        ser.write('P1,S6,mmm.nnn\r\n'.encode('utf-8'))        
         data = ser.read(10)  # 读取10个字节
+        print(f'压力设置 respnse: {data}\n')
+        continue
     else:
         print('Invalid input. Please try again.')  
 
